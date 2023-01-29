9d98dd05049f2cd706cbc9764d979739dc20b290
Weiyi Lee <weiyilee17@gmail.com>
2023-01-29 13:08:41 +0800

    Rendered the app titles in 2 columns

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
-from streamlit import columns, image, title, info, set_page_config, info
+from streamlit import columns, image, title, write, set_page_config, info, header
+from pandas import read_csv
 
 set_page_config(layout='wide')
 
-left_column, right_column = columns(2)
+photo_column, self_introduction_column = columns(2)
 
-with left_column:
+with photo_column:
     image('images/photo.png')
 
-with right_column:
+with self_introduction_column:
     title('Weiyi Lee')
     self_introduction = """
     Hi! I'm Lee. I'm a React front end engineer working in LITEON. 
     I have experience building mid-large scale web sites using React, TypeScript, redux, redux-saga, and webpack. 
-    Currently I'm learning python. The photo on the left is Ardit Sulce, my python teacher.
+    Currently I'm learning Python. The photo on the left is Ardit Sulce, my Python teacher.
     """
     info(self_introduction)
+
+app_introduction = """
+Below you can find some of the apps I've built in Python. Feel free to contact me! 
+Place holders to see if they can pass the limit of the photo above.
+"""
+
+write(app_introduction)
+
+app_left_hand_side_column, app_right_hand_side_column = columns(2)
+
+apps = read_csv('data.csv', sep=';')
+
+for index, row in apps.iterrows():
+    if index % 2 == 0:
+        with app_left_hand_side_column:
+            header(row['title'])
+    else:
+        with app_right_hand_side_column:
+            header(row['title'])
+
 
